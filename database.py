b70fbdd2ebf6eb6a23a1c052074a500cc66bc112
Danaa Younus <danaayounus@gmail.com>
2025-03-18 23:34:49 +0500

    alembic migrations done issue fixed

--- database.py
+++ database.py
@@ -5,14 +5,15 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 dotenv_path = find_dotenv()
-# print(f"Loading .env from: {dotenv_path}")  # Debugging step
 load_dotenv(dotenv_path, override=True)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-print(f"Database URL: {DATABASE_URL}")  # Debugging step
+# print(f"Database URL: {DATABASE_URL}")  # Debugging step
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
+
+from models import user, post, subreddit, comment, vote, message, notification, user_subreddit
 
 def get_db():
     db = SessionLocal()
